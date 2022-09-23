1a335ccb63c88fb17e3c8fa57111a7b135ea9440
Mifaresss <9803211@gmail.com>
2022-09-23 17:12:05 +0300

    The initial view of the form has been created. Received and added cards to the DOM

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function getData() {
+	const requestURL = 'https://randomuser.me/api/?results=50';
+	return fetch(requestURL)
+		.then(response => {
+			if (!response.ok) {
+				throw Error(response.statusText);
+			}
+			return response.json();
+		})
+		.then(data => data.results)
+}
+
+function createPeople(person) {
+	const human = document.createElement('li');
+	human.classList.add('people__human')
+	human.innerHTML = `
+	<img class='people__img' src='${person.picture.large}'>
+	<h3 class='people__name'>Меня зовут ${person.name.first}</h3>	
+	<p class='people__age'>Мне ${person.dob.age}</p>
+	`
+	return human;
+}
+
+const peopleList = document.querySelector('.people');
+function createPeopleHTML() {
+	getData().then(people => {
+		const peopleHtml = people.map(human => createPeople(human))
+		peopleList.append(...peopleHtml)
+	})
+}
+createPeopleHTML();
