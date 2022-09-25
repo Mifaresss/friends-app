0942c83b4b779f881918eb556e2e66b8a67ffa8d
Mifaresss <9803211@gmail.com>
2022-09-25 23:26:54 +0300

    Improved site view. Added the logic of the filter by gender

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
+let newPersonGender;
 function getData() {
-	const requestURL = 'https://randomuser.me/api/?results=52&inc=name,gender,picture,dob';
+	const requestURL = 'https://randomuser.me/api/?results=54&inc=name,gender,picture,dob';
 	return fetch(requestURL)
 		.then(response => {
 			if (!response.ok) {
@@ -24,28 +25,29 @@ function createHuman(person) {
 	return human;
 }
 
-const peopleList = document.querySelector('.people');
+const peopleList = document.querySelector('.people__list');
 let receivedPeople = [];
 let filteredPeople = [];
 
 function createPeopleHTML() {
 	getData().then(people => {
-		receivedPeople = [...people];
+		receivedPeople = people.slice();
 		renderPeople(receivedPeople);
 	})
 }
 createPeopleHTML();
 
 function renderPeople(people) {
+	peopleList.innerHTML = '';
 	const peopleHtml = people.map(human => createHuman(human))
 	peopleList.append(...peopleHtml)
 }
 
 
 function convertGender(person) {
-	if (person.gender == 'male') person.gender = './images/male-svgrepo-com.svg';
-	if (person.gender == 'female') person.gender = './images/female-svgrepo-com.svg';
-	return person.gender;
+	if (person.gender == 'male') newPersonGender = './images/male-svgrepo-com.svg';
+	if (person.gender == 'female') newPersonGender = './images/female-svgrepo-com.svg';
+	return newPersonGender;
 }
 function coloringCard(person, human) {
 	if (person.gender == 'male') human.classList.add('people__human_male')
@@ -62,4 +64,15 @@ form.addEventListener('change', filterData);
 function filterData({ currentTarget }) {
 	const checkedInput = Array.from(currentTarget.elements.gender).find(gender => gender.checked);
 	const checkedGender = checkedInput.id;
+	const filteredData = filterByGender(checkedGender, receivedPeople);
+	renderPeople(filteredData);
 }
+
+function filterByGender(checked, people) {
+	if (checked === 'all') return people;
+	filteredPeople = people.filter(human => {
+		if (human.gender == checked) return human;
+	});
+	return filteredPeople;
+}
+
